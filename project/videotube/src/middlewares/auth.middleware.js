629c7ba44e80b79a17fd6398e780b3a45a201670
mr-vsr <vikasraivsr630@gmail.com>
2024-03-21 13:07:08 +0530

    added changePassword , avatarUpdat, coverImageUpdate, updateDetails, refreshToken controllers and routes

--- project/videotube/src/middlewares/auth.middleware.js
+++ project/videotube/src/middlewares/auth.middleware.js
@@ -5,6 +5,7 @@ import { User } from "../models/user.model.js";
 
 export const verifyJWT = asyncHandler(async (req, res,next) => {
     try {
+
         const token = req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ", "");
 
         if (!token) {
@@ -20,6 +21,7 @@ export const verifyJWT = asyncHandler(async (req, res,next) => {
         }
 
         req.user = user;
+        
         next();
 
     } catch (error) {
